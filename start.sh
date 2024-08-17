b218044a6119b249a9931d4cd490de3c6ca03ab7
yuuaHP <identity@yuua.dev>
2024-08-17 19:57:03 +0900

    feat: build rejao-afa on docker

--- start.sh
+++ start.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# build rejao-afa
 cd ./rejao-afa || exit
-./gradlew build
+docker compose build
+docker compose up
+
 cd ..
 mv ./rejao-afa/build/libs/rejao-afa-1.0-SNAPSHOT-all.jar ./plugins/rejao-afa-1.0-SNAPSHOT-all.jar
 docker compose build
